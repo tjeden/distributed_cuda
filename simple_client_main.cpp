950a10d424be81e4b551816d9d9e7e85c5db1030
Aleksander Dabrowski <aleks@kumulator.pl>
2010-12-04 17:39:52 +0100

    brutal working wersion

--- simple_client_main.cpp
+++ simple_client_main.cpp
@@ -12,25 +12,19 @@ int main ( int argc, int argv[] ) {
 
   try 
   {
+    std::string reply;
+    std::stringstream client_port_string;
+    std::stringstream buff;
+
     ClientSocket client_socket ( "localhost", 5555 );
 
-    std::string reply;
-
-    std::stringstream client_port_string;
     client_port_string << client_port;
     client_socket << "REGISTER " << client_ip << " " << client_port_string.str() << " foo";
     client_socket >> reply;
 
+    client_number = reply;
     std::cout << "We received this response from the server:\n" << reply << "\n";;
 
-  }
-  catch ( SocketException& e )
-  {
-    std::cout << "Exception was caught:" << e.description() << "\n";
-  }
-
-  try 
-  {
     std::cout << "Starting listener" << std::endl;
     ServerSocket server (client_port);
     std::cout << "Listener started" << std::endl;
@@ -47,8 +41,13 @@ int main ( int argc, int argv[] ) {
           std::cout << "1\n";
           new_sock >> data;
           std::cout << "2\n";
-          new_sock << data;
+          new_sock << "OK";
           std::cout << "3\n";
+          buff << "RESPONSE " << client_number << " " << data;
+          client_socket << buff.str();
+          std::cout << "4\n";
+          client_socket >> reply;
+          std::cout << "5\n";
         }
       }
       catch ( SocketException& e) 
