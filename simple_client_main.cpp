681eefe9b0c257ece2d1d1f2fb87e850f60887c4
Aleksander Dabrowski <aleks@kumulator.pl>
2010-12-04 12:27:25 +0100

    clients now reigster itself

--- simple_client_main.cpp
+++ simple_client_main.cpp
@@ -1,25 +1,42 @@
 #include "ClientSocket.h"
+#include "ServerSocket.h"
 #include "SocketException.h"
 #include <iostream>
 #include <string>
 
-int main ( int argc, int argv[] )
-{
-  try
-    {
+int main ( int argc, int argv[] ) {
+  std::string client_ip = "127.0.0.1";
+  std::string client_port = "9999";
+  std::string client_number;
+
+  try {
+    ServerSocket server ( 30000 );
+      while ( true ){
+        ServerSocket new_sock;
+        server.accept ( new_sock );
+        try {
+          while ( true ) {
+            std::string data;
+            new_sock >> data;
+            new_sock << data;
+          }
+        }
+        catch ( SocketException& ) {}
+      }
 
       ClientSocket client_socket ( "localhost", 5555 );
 
       std::string reply;
 
       try
-	{
-	  client_socket << "Test message.";
-	  client_socket >> reply;
-	}
+        {
+          client_socket << "REGISTER " << client_ip << " " << client_port << " foo";
+          client_socket >> reply;
+        }
       catch ( SocketException& ) {}
 
       std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+      client_number = reply;
 
     }
   catch ( SocketException& e )
