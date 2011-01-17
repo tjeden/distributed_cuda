d557049377c3dab0dee4187078acee91b6ada421
Aleksander Dabrowski <aleks@kumulator.pl>
2011-01-17 21:29:07 +0100

    client class created

--- simple_client_main.cpp
+++ simple_client_main.cpp
@@ -6,6 +6,69 @@
 #include <sstream>
 #include <vector>
 
+class client 
+{
+  public:
+    client (std::string, int, std::string);
+
+    std::string ip;
+    std::string task;
+    std::string number;
+    int port;
+
+    std::string register_in_server(void);
+    std::string accept_data();
+    std::string send_data(std::string);
+};
+
+client::client (std::string aIp, int aPort, std::string aTask)
+{
+  client::ip = aIp; 
+  client::port = aPort;
+  client::task = aTask;
+};
+
+std::string client::register_in_server(void)
+{
+  ClientSocket client_socket ( "localhost", 5555 );
+
+  std::string reply;
+  std::stringstream client_port_string;
+  client_port_string << port;
+
+  client_socket << "REGISTER " << ip << " " << client_port_string.str() << " " << task;
+  client_socket >> reply;
+  number = reply;
+
+  return reply;
+};
+
+std::string client::accept_data()
+{
+  std::string data = "";
+  ServerSocket server (port);
+  ServerSocket new_sock;
+  std::cout << "Socket accepted\n";
+  server.accept ( new_sock );
+  new_sock >> data;
+  new_sock << "OK";
+  new_sock.close();
+
+  return data;
+};
+
+std::string client::send_data(std::string data)
+{
+  ClientSocket client_socket ( "localhost", 5555 );
+  std::string reply;
+  std::string buff;
+  buff = "RESPONSE " + number + " " + data;
+  client_socket << buff;
+  client_socket >> reply;
+
+  return reply;
+}
+
 extern "C" int calculate(const char *, char*);
 
 int main ( int argc, int argv[] ) {
@@ -15,50 +78,35 @@ int main ( int argc, int argv[] ) {
 
   try 
   {
+
     std::string reply;
     std::stringstream client_port_string;
-    std::string buff;
 
-    ClientSocket client_socket ( "localhost", 5555 );
+    client simple_client(client_ip, client_port, "foo");
+    client_number = simple_client.register_in_server();
 
-    client_port_string << client_port;
-    std::cout << "Update?\n";
-    client_socket << "REGISTER " << client_ip << " " << client_port_string.str() << " vector_add";
-    client_socket >> reply;
-
-    client_number = reply;
-    std::cout << "We received this response from the server:\n" << reply << "\n";;
+    std::cout << "We received this response from the server:\n" << client_number << "\n";;
 
     std::cout << "Starting listener" << std::endl;
-    ServerSocket server (client_port);
     std::cout << "Listener started" << std::endl;
     while ( true )
     {
-      ServerSocket new_sock;
-      std::cout << "Socket accepted\n";
       try 
       {
         while ( true ) 
         {
-          server.accept ( new_sock );
           std::string data = "";
-          char result[10];
-          //std::cout << "1\n";
-          new_sock >> data;
+          data = simple_client.accept_data();
           std::cout << data.length() << "\n";
-          //std::cout << "2\n";
-          new_sock << "OK";
-          new_sock.close();
-          //std::cout << "3\n";
-          std::cout << "Processing.\n";
-          calculate(data.c_str(), result);
+
+          //Temporary
+          //std::cout << "Processing.\n";
+          //char result[10];
+          //calculate(data.c_str(), result);
           //result[0] = 'd';
-          std::cout << "Done.\n";
-          buff = "RESPONSE " + client_number + " " + result;
-          client_socket << buff;
-          //std::cout << "4\n";
-          client_socket >> reply;
-          //std::cout << "5\n";
+          //std::cout << "Done.\n";
+
+          simple_client.send_data(data);
         }
       }
       catch ( SocketException& e) 
